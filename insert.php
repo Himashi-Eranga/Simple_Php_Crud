d5c25b18b423f56f908556d69bd910504e0b4398
IT22604958 <IT22604958@my.sliit.lk>
2025-06-28 09:44:51 +0530

    create the form

---
+++ insert.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Account</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Insert user Details</h1>
+    <form>
+        <lable>User Name:</lable>
+        <input type="text"></br></br>
+        <lable>User Gamil:</lable>
+        <input type="email"></br></br>
+        <lable>Phone Number:</lable>
+        <input type="text"></br></br>
+        <lable>User Address:</lable>
+        <input type="text"></br></br>
+        <lable>Password:</lable>
+        <input type="password"></br></br>
+        <button type="submit">Submit</button>
+
+    </form>
+</body>
+</html>
